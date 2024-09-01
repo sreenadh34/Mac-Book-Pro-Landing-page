b79361b573178ae3b1b39cda95256d9acd87abb9
sreenadh34 <43.sreenadh@gmail.com>
2024-09-01 23:41:42 +0530

    update scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 import "./style.css"
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
 import MacContainer from "./MacContainer";
 
 const App = () => {
   return(
+    
     <Canvas camera ={{ fov: 12, position: [0, -10, 220]}}>
       <OrbitControls />
       <Environment 
       files={[
         "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr"
         ]} />
-      <MacContainer />
+      <ScrollControls pages={3}>
+        <MacContainer />
+      </ScrollControls>
+      
     </Canvas>
   )
 }
--- src/MacContainer.jsx
+++ src/MacContainer.jsx
@@ -1,12 +1,35 @@
-import { useGLTF } from "@react-three/drei"
+import { useGLTF, useScroll, useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 const MacContainer = () => {
-  let model = useGLTF("./mac.glb");
+  const model = useGLTF("./mac.glb");
+  const tex = useTexture("./red.jpg");
+
+  const meshes = {};
+  model.scene.traverse((e) => {
+    meshes[e.name] = e;
+  });
+
+  // Initialize the mesh properties
+  meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+  meshes.matte.material.map = tex;
+  meshes.matte.material.emissiveIntensity = 0;
+  meshes.matte.material.metalness = 0;
+  meshes.matte.material.roughness = 1;
+
+  const data = useScroll();
+
+  useFrame(() => {
+    // Update rotation based on scroll offset
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - data.offset * 90);
+  });
+
   return (
-    <group position={}>
-     <primitive object={model.scene} />;
-     </group>
-  )
+    <group position={[0, -10, 20]}>
+      <primitive object={model.scene} />
+    </group>
+  );
 };
 
-export default MacContainer
+export default MacContainer;
